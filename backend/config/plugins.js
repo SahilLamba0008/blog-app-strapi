1e11fe4793ade777f135d571103447d80f48deaa
SahilLamba0008 <19bcs1922@gmail.com>
2024-09-22 12:34:12 +0530

    Update node version and plugins.js for vercel deployment

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,5 +1,12 @@
 module.exports = ({ env }) => ({
     "vercel-deploy": {
       enabled: true,
+      config: {
+        deployHook: "",
+        apiToken: "",
+        appFilter: "",
+        teamFilter: "",
+        roles: ["",""]
+      },
     },
   });
